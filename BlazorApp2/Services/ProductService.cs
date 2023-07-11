6f40da338c1d8ac73a33c3be15a1f08a70b67639
Samantha Nicole Tipanguano Santamaria <75260906+sntipanguano@users.noreply.github.com>
2023-07-10 22:50:05 -0500

    testinter

---
+++ BlazorApp2/Services/ProductService.cs
@@ -0,0 +1,46 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace BlazorApp2;
+
+public class ProductService : IProductService
+{
+    private readonly HttpClient client;
+
+    private readonly JsonSerializerOptions options; 
+
+    public ProductService( HttpClient httpClient)
+    {
+        client = httpClient;
+        options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+
+        public async Task<List<Product>?> Get()
+        {
+            var response = await client.GetAsync("v1/products");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            return JsonSerializer.Deserialize<List<Product>>(content, options);
+        }
+
+        public async Task Delete(int productId)
+        {
+            var response = await client.DeleteAsync($"v1/products/{productId}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+}
+
+ public interface IProductService
+    {
+        Task<List<Product>?> Get();
+
+        Task Delete(int productId);
+    }
